6825d25f3704e3d5bc8e7a4c9efec46e411c1b64
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-05-31 22:10:55 +0200

    Bonus 1

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ class Movie{
     // proprietà
     public $name;
     public $year_of_release;
-    public $category;
+    public $category=[];
     public $director;
 
     // costruttore
-    function __construct($name,$year_of_release,$category,$director){
+    function __construct($name,$year_of_release,array $category,$director){
         $this->name = $name;
         $this->year_of_release = $year_of_release;
         $this->category = $category;
@@ -17,16 +17,20 @@ class Movie{
 
     // metodi
     function printFilm(){
-        echo "$this->name è un film del $this->year_of_release di genere $this->category diretto da $this->director <br>";
+        echo "$this->name è un film del $this->year_of_release di genere ";
+        foreach($this->category as $key => $value){
+            echo "$value ";
+          }
+        echo " diretto da $this->director <br>";
     }
 }
 
 // istanza 1
-$film1 = new Movie("Pulp Fiction","1994","Crime/Drama","Quentin Tarantino");
+$film1 = new Movie("Pulp Fiction","1994",["Crime", "Drama","Thriller", "Comedy"],"Quentin Tarantino");
 $film1->printFilm();
 
 // istanza 2
-$film2 = new Movie("Jurassic Park","1993","Adventure","Steven Spielberg");
+$film2 = new Movie("Jurassic Park","1993",["Adventure", "Fantasy"],"Steven Spielberg");
 $film2->printFilm();
 
 
